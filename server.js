319aa6afbe69a949c9bd08851e505c9c3beb9c57
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-15 21:00:38 +0300

    not yet finished

--- server.js
+++ server.js
@@ -4,42 +4,86 @@ const server = express();
 const weather = require('./data/weather.json');
 
 const cors = require('cors');
+const { response } = require('express');
 server.use(cors()); 
 
 
-const PORT = 3111;
+const PORT = process.env.PORT || 3030;
+ require("dotenv").config();
 
-// localhost:3111/test
+// localhost:3030/test
 // server.get('/test',(req, res)=>{
 //   res.send('hello');
 // })
 
-// localhost:3111/weatherData
+// localhost:3030/weatherData
 server.get('/weatherData', (req, res)=>{
     res.send(weather);
 })
 
-// localhost:3111/lon-lat
+// localhost:3030/lon-lat
 server.get('/lon-lat', (req, res)=>{
-    let dataArr =[weather.lon, weather.lat]; 
+    let dataArr =weather.map((city)=> {
+        return [`longitude: ${city.lon}` , `latitude: ${city.lat}`];
+    }) 
     res.send(dataArr);
 })
 
-// localhost:3111/searchCity?lon=151.21&lat=-33.87
+// localhost:3030/searchCity?lon=2.35&lat=48.86
 server.get('/searchCity', (req, res)=>{
     let cityLon = req.query.lon;
+    // console.log(`${cityLon} long,`);
     let cityLat = req.query.lat;
-    let findCity =()=>{
-    if(weather.lon === cityLon && weather.lat === cityLat){
-        res.send(findCity.city_name);
-      } 
-    else  {
-        res.status(500);
-        res.send("ERROR:NOT FOUND");
-     }
+    // console.log(`${cityLat} lat,`);
+//    console.log(weather);
+     let  cityFound =  weather.find(function(city){
+        //    console.log(cityLon);
+        //   console.log("find", city);
+          return (city.lon == cityLon && city.lat == cityLat);
+       
+
+     });
+    //    console.log("getting city from find",cityFound);
+    if ( cityFound){
+        res.status=200;
+        res.send(cityFound.city_name);
     }
-    
+    else {
+        res.status =500;
+        res.send("ERROR NOT FOUND");
+    }
+
+   
 })
+
+
+// localhost:3030/dated-data
+server.get('/dated-data',(req,res)=>{
+    let datedDataArr = weather.data;
+    res.send(datedDataArr);
+    // console.log(datedDataArr);
+})
+
+// localhost:3030/forcastEachCity
+server.get('/forcastEachCity', (req, res)=>{
+
+    let forcastData = weather.map((city)=>{
+        
+        return new Forecast (city);
+    })
+    res.send(forcastData);
+    console.log (forcastData);
+
+});
+
+
+
+class Forecast{
+    constructor(city){
+        this.date= city.data.valid_date,
+        this.description= city.data.weather.description
+    }
+}
 
 server.listen(PORT, ()=>{
     console.log(`Listening to PORT ${PORT} o.O`);
