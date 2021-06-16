f1bcc8148843b31653227b7f14b4d39317350466
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-16 12:38:23 +0300

    back end done

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 3030;
  require("dotenv").config();
 
 // localhost:3030/test
-// server.get('/test',(req, res)=>{
-//   res.send('hello');
-// })
+server.get('/test',(req, res)=>{
+  res.send('hello');
+})
 
 // localhost:3030/weatherData
 server.get('/weatherData', (req, res)=>{
@@ -29,59 +29,55 @@ server.get('/lon-lat', (req, res)=>{
     res.send(dataArr);
 })
 
-// localhost:3030/searchCity?lon=2.35&lat=48.86
+// localhost:3030/searchCity?cityName=amman
 server.get('/searchCity', (req, res)=>{
-    let cityLon = req.query.lon;
-    // console.log(`${cityLon} long,`);
-    let cityLat = req.query.lat;
-    // console.log(`${cityLat} lat,`);
-//    console.log(weather);
-     let  cityFound =  weather.find(function(city){
-        //    console.log(cityLon);
-        //   console.log("find", city);
-          return (city.lon == cityLon && city.lat == cityLat);
-       
+    let cityName = req.query.cityName;
+    console.log(cityName);
 
-     });
-    //    console.log("getting city from find",cityFound);
-    if ( cityFound){
-        res.status=200;
-        res.send(cityFound.city_name);
-    }
-    else {
-        res.status =500;
-        res.send("ERROR NOT FOUND");
-    }
-
-   
-})
-
-
-// localhost:3030/dated-data
-server.get('/dated-data',(req,res)=>{
-    let datedDataArr = weather.data;
-    res.send(datedDataArr);
-    // console.log(datedDataArr);
-})
-
-// localhost:3030/forcastEachCity
-server.get('/forcastEachCity', (req, res)=>{
-
-    let forcastData = weather.map((city)=>{
-        
-        return new Forecast (city);
+    let cityFound = weather.find((city)=>{
+        let name = city.city_name;
+        // console.log(`name: ${name}`);
+        if (name.toLocaleLowerCase() == cityName.toLocaleLowerCase()){
+        // console.log("inside find");
+        return city;}   
     })
-    res.send(forcastData);
-    console.log (forcastData);
+    // console.log(cityFound);
+    if(cityFound){
+        res.status = 200;
+        res.send(`City: ${cityFound.city_name}  -  Longitude: ${cityFound.lon}  -  Latitude: ${cityFound.lat}`);
+    }else if (! cityFound){
+        res.status =500;
+        res.send(`ERROR: DATA NOT FOUND FOR REQUIRED REGION`);
+    }
+})
 
-});
+// localhost:3030/cityData?cityName=paris
+server.get('/cityData', (req, res)=>{
+  let cityName = req.query.cityName;
+  let cityValidation = weather.find((city)=>{
+      return (city.city_name.toLocaleLowerCase() == cityName.toLocaleLowerCase());
+  })
+//   console.log("cityValidation");
+
+  if (cityValidation){
+    //   console.log("inside if")
+    let cityData = cityValidation.data.map(day => new Forecast(day));
+    console.log(cityData);
+      res.status=200;
+      res.send(cityData);
+  }
+  else{
+    res.status =500;
+    res.send(`ERROR: DATA NOT FOUND FOR REQUIRED REGION`);
+  }
+})
 
 
 
 class Forecast{
     constructor(city){
-        this.date= city.data.valid_date,
-        this.description= city.data.weather.description
+        this.date= city.valid_date,
+        this.description= city.weather.description
     }
 }
 
@@ -90,3 +86,31 @@ server.listen(PORT, ()=>{
 })
 
 
+
+
+// // localhost:3030/searchCity?lon=2.35&lat=48.86
+// server.get('/searchCity', (req, res)=>{
+//     let cityLon = req.query.lon;
+//     // console.log(`${cityLon} long,`);
+//     let cityLat = req.query.lat;
+//     // console.log(`${cityLat} lat,`);
+// //    console.log(weather);
+//      let  cityFound =  weather.find(function(city){
+//         //    console.log(cityLon);
+//         //   console.log("find", city);
+//           return (city.lon == cityLon && city.lat == cityLat);
+       
+
+//      });
+//     //    console.log("getting city from find",cityFound);
+//     if ( cityFound){
+//         res.status=200;
+//         res.send(cityFound.city_name);
+//     }
+//     else {
+//         res.status =500;
+//         res.send("ERROR NOT FOUND");
+//     }
+
+   
+// })
