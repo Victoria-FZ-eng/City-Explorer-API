a61d8e642634f0f0cbc81ad55fb4642be341f001
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-17 16:56:53 +0300

    lab 09 back-end done

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **Author**: Victoria Fares Abu Al-Zulof **Version**: 1.0.0
 
 ## Overview
-This is an api server for my city explorer app. getting the weather data for the searched region.
+This is an api server for my city explorer app. getting the weather and  movie data for the searched region.
 
 ## Getting Started
 Enter the name of the city or region you want to display the map for, and then click on Explore.
@@ -16,9 +16,12 @@ This is build as a server using node js and express.
 15th June , lab07 (completing the lab).
 16th June, lab07 (DONE).
 17th June, lab08 (back-end almost done).
-18th June, lab08 (back-end done).
+17th June, lab08 (back-end done).
+17th June, lab09 (REFACTORING)
+
 
 ## Credit and Collaborations
 
 TA. Anolla Haddad.
 TA. Aisha.
+TA. Hanaa
---
+++ movie.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+
+class Movies {
+    constructor(film){
+      this.poster = film.poster_path,
+      this.name = film.original_title,
+      this.description = film.overview,
+      this.date = film.release_date,
+      this.pop = film.popularity
+    }
+  }
+const mv = function movies (req, res){
+    let location = req.query.location;
+    let movieKey= process.env.MOVIE_API_KEY;
+    let movieURL= `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${location}`;
+  
+    axios
+      .get(movieURL).then(movies=>{
+        
+        const moviesObjects = movies.data.results.map(obj=> new Movies (obj));
+        
+        if( moviesObjects.length != 0){
+        res.send(moviesObjects);
+        console.log(moviesObjects);}
+        else{
+            res.status(500).send(`${err}: MOVIE'S DATA NOT FOUND FOR REQUIRED LOCATION`);
+            console.log("not valid");
+        }  
+      })
+      .catch(err => {
+        res.status(500).send(`${err}: MOVIE'S DATA NOT FOUND FOR REQUIRED LOCATION`);
+        console.log("catch");
+    })
+  };
+
+  module.exports = mv;
